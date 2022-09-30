b48899689daa15f159bac1a350215579fa5e1d86
Alka04 <alka04006@utb.vaxjo.se>
2022-09-30 10:17:43 +0200

    Add project files.

---
+++ kapitel 3 uppgift/Program.cs
@@ -0,0 +1,29 @@
+﻿using System; 
+namespace Uppgift3_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder >= 16 && ålder <= 20)
+            {
+                Console.WriteLine("Du får vara med och tävla");
+            }
+
+            else 
+            {
+                Console.WriteLine("Du är får inte vara med"); 
+            }
+
+            
+
+            
+        }
+    }
+}
+
+
+
